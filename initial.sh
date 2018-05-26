ca8cb82cced906569dd496b9e4bd03f0f4603639
shrimp <liu.shrimp@gmail.com>
2018-05-26 21:20:57 +0800

    change

--- initial.sh
+++ initial.sh
@@ -1,12 +1,37 @@
 #!/bin/sh
+
+echo "Alias cleos"
 alias cleos='docker-compose exec keosd cleos -u http://nodeosd:8888 --wallet-url http://localhost:8888'
+
+public_key=EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+private_key=5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+
+echo "Import Wallet"
 cleos wallet create > wallet.info
-cleos create account eosio eosio.bios EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos set contract eosio.bios data-dir/contracts/eosio.bios -p eosio.bios
-cleos create account eosio eosio.msig EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos set contract eosio.msig data-dir/contracts/eosio.msig -p eosio.msig
-cleos create account eosio eosio.token EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+# cleos wallet import ${private_key}
+sleep 1
+
+echo "Create System Accounts"
+system_accounts=(eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.vpay)
+for acc in ${system_accounts[@]}
+do
+    cleos create account eosio ${acc} ${public_key} ${public_key}
+done
+sleep 1
+
+echo "Install System Contracts"
 cleos set contract eosio.token data-dir/contracts/eosio.token -p eosio.token
+cleos set contract eosio.msig data-dir/contracts/eosio.msig -p eosio.msig
+sleep 1
+
+echo "Create Tokens"
 cleos push action eosio.token create '{"issuer":"eosio", "maximum_supply":"1000000000.0000 SYS", "can_freeze":0, "can_recall":0, "can_whitelist":0}' -p eosio.token
 cleos push action eosio.token issue '["eosio","1000000000.0000 SYS","deposit started"]' -p eosio
+sleep 1
+
+echo "Set System Contract"
 cleos set contract eosio data-dir/contracts/eosio.system -p eosio
+sleep 1
+cleos push action eosio setpriv '["eosio.msig", 1]' -p eosio@active
+sleep 1
+
